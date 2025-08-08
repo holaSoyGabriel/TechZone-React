de81dc8bfe66a0f03fd0b50e7cb4e1d8364deb85
holaSoyGabriel <gabriel.hd1206@gmail.com>
2025-08-08 10:26:05 -0400

    quite la imagen del banner que de verdad me moestaba muchisimo

--- src/components/HeroBanner/HeroBanner.jsx
+++ src/components/HeroBanner/HeroBanner.jsx
@@ -4,7 +4,7 @@ import "./HeroBanner.css";
 const HeroBanner = () => {
     return (
         <div className="hero-banner">
-            <img src="/src/assets/banner.jpg" alt="Promo banner" className="hero-image" />
+
             <div className="hero-text">
                 <h1>Bienvenido a TechZone</h1>
                 <p>Descubre las mejores ofertas hoy</p>
